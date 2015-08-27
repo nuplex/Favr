66b714e955f5d5a0afa43d0688c9754660afc02f
Chijioke Okonkwo <chokon@umd.edu>
2015-08-26 22:23:12 -0400

    User stuff

--- app/src/main/java/co/favrapp/favr/favr/backend/User.java
+++ app/src/main/java/co/favrapp/favr/favr/backend/User.java
@@ -1,7 +1,33 @@
 package co.favrapp.favr.favr.backend;
 
+import java.util.ArrayList;
+
 /**
  * Created by chijioke on 8/21/15.
  */
 public class User {
+
+    //nothing here is final
+
+    /**
+     * Pertinent Facebook Data, such as first name, last name, email
+     */
+    public String firstName;
+    private String lastName;
+    public String username;
+    public String email;
+
+    public String phoneNumber; //this might be an object rather than a string
+
+    private boolean isVerified; //phone number verified
+
+    private int favrsPickedUp;
+    private int favrsDropped;
+    private int favrsCanceled;
+    private int favrsCompleted;
+    private int favrsCreated;
+
+    private ArrayList<Favr>
+
+
 }
